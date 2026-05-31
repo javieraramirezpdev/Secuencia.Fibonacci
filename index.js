9b93cb1518b4fb29fbee8aa61df7f7f035cc5359
Javiera Ramírez <javiera.ramirezp.dev@gmail.com>
2026-05-31 12:22:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-//Mostrar en consola los números del 1 al 1000, indicando pares e impares.
-let fibonacci = [0, 1];
-
-while (true) {
-    let siguiente = fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
-    if (siguiente > 1000) {
-        break;
-    }   
-    fibonacci.push(siguiente);
-}
-
-console.log("Secuencia de Fibonacci entre 0 y 1000:");
-console.log(fibonacci);
-
-console.log("Números pares entre 0 y 1000:");
-console.log(fibonacci.filter(num => num % 2 === 0));
-
-console.log("Números impares entre 0 y 1000:");
-console.log(fibonacci.filter(num => num % 2 !== 0));
-
 // EJERCICIO 1
 // Fibonacci entre 0 y 1000
 
